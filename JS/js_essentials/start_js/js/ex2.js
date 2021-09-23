037001aaae57ed22389967110234ed03322c383a
dudcks5477 <dudcks5477@naver.com>
2021-09-23 17:10:48 +0900

    Arithmetic & assignment operator

---
+++ JS/js_essentials/start_js/js/ex2.js
@@ -0,0 +1,21 @@
+// 산술 연산자(arithmetic operator)
+
+console.log(1 + 2)
+console.log(3 - 4)
+console.log(5 * 6)
+console.log(14 / 7)
+console.log(15 % 7)
+
+console.log('----')
+
+// 할당 연산자(assignment operator)
+
+let a = 2
+// a = a + 1
+// a += 1
+// a -= 1
+// a *= 1
+// a /= 1
+a %= 1
+
+console.log(a)
