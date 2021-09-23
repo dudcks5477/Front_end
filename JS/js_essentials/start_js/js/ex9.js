ac776015671e3648a9db7dfa01d1feac2d02aa7d
dudcks5477 <dudcks5477@naver.com>
2021-09-23 21:17:29 +0900

    Type conversion

---
+++ JS/js_essentials/start_js/js/ex9.js
@@ -0,0 +1,16 @@
+// 형 변환(Type conversion)
+
+const a = 1
+const b = '1'
+
+console.log(a === b)
+console.log(a == b) // 동등 연산자 (의도하지 않은 결과값이 나올 수 있음)
+
+console.log('----')
+
+if (true) {
+  console.log(123)
+}
+if (false) {
+  console.log(123)
+}
