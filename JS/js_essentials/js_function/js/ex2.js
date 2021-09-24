579b5e2b8e7cf02ae71eca7d890683a8158f3ef5
dudcks5477 <dudcks5477@naver.com>
2021-09-24 13:18:08 +0900

    Arrow Function

---
+++ JS/js_essentials/js_function/js/ex2.js
@@ -0,0 +1,9 @@
+// 화살표 함수
+
+const double = function (x, y) {
+  return x * 2
+}
+console.log('double: ', double(7))
+
+const doubleArrow = x => ({ name: 'CHAN' })
+console.log('doubleArrow', doubleArrow(7))
