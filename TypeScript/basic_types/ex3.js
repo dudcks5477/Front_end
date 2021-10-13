2bc7061edce55b95df04daa5b52939d7d4d3aade
dudcks5477 <dudcks5477@naver.com>
2021-10-13 11:27:46 +0900

    String

---
+++ TypeScript/basic_types/ex3.js
@@ -0,0 +1,8 @@
+"use strict";
+// let myname: string = "CHAN";
+// myname = 'SEOCK';
+// Template String
+var fullName = "Kim Chan";
+var age = 24;
+var sentence = "Hello, my name is " + fullName + ".\n\nI'll be " + (age + 1) + " years old next month.";
+console.log(sentence);
---
+++ TypeScript/basic_types/ex3.ts
@@ -0,0 +1,13 @@
+// let myname: string = "CHAN";
+
+// myname = 'SEOCK';
+
+// Template String
+let fullName: string = "Kim Chan";
+let age: number = 24;
+
+let sentence: string = `Hello, my name is ${fullName}.
+
+I'll be ${age + 1} years old next month.`;
+
+console.log(sentence);
