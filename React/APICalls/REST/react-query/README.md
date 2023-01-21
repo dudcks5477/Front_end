a429124fa7d9e36b1a8d87b2440f5db90f5f22b3
dudcks5477 <dudcks5477@naver.com>
2023-01-21 13:18:02 +0900

    React Query

--- React/APICalls/REST/react-query/README.md
+++ React/APICalls/REST/react-query/README.md
@@ -1,1 +1,20 @@
 # [react-query](https://www.youtube.com/playlist?list=PLC3y8-rFHvwjTELCrPrcZlo6blLBUspd2)
+
+## Project Setup
+1. New react projcet using CRA
+```bash
+npx create-react-app react-query-demo
+```
+2. Set up an API endpoint that serves mock data for use in our application
+    - [JSON Server](https://npmjs.com/package/json-server)
+    ```bash
+    npm install json-server
+    ```
+3. Set up react router and a few routes in the application
+```bash
+npm install react-router-dom
+```
+4. Fetch data the traditional way using useEffect and useState
+```bash
+npm install axios
+```
---
+++ React/APICalls/REST/react-query/react-query-demo/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { SuperHeroesPage } from './components/SuperHeroes.page';
+import { RQSuperHeroesPage } from './components/RQSuperHeroes.page';
+import { HomePage } from './components/Home.page';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/super-heroes'>Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path='/super-heroes'>
+            <SuperHeroesPage />
+          </Route>
+          <Route path='/rq-super-heroes'>
+            <RQSuperHeroesPage />
+          </Route>
+          <Route path='/'>
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
